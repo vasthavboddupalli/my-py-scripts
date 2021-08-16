06126e24136aaecd655231e23c1b003fb1177137
Vasthav Boddupalli <60834832+vasthavboddupalli@users.noreply.github.com>
2021-08-16 09:47:33 -0400

    disabling the fail-safe to not logout before duration

--- alive.py
+++ alive.py
@@ -2,5 +2,6 @@ import pyautogui
 import sys
 import time
 from datetime import datetime
+pyautogui.FAILSAFE = False
 print("Vasthav made Movement at {}".format(datetime.now()))
 pyautogui.moveTo(100,200, duration=3600)
