e3bd1badc7083462395c81f1e6c229a59984ffd9
Vasthav Boddupalli <60834832+vasthavboddupalli@users.noreply.github.com>
2021-08-16 09:56:41 -0400

    remove personal name

--- alive.py
+++ alive.py
@@ -3,5 +3,5 @@ import sys
 import time
 from datetime import datetime
 pyautogui.FAILSAFE = False
-print("Vasthav made Movement at {}".format(datetime.now()))
+print("Movement made at {}".format(datetime.now()))
 pyautogui.moveTo(100,200, duration=3600)
